d14fa0c70b54c21ec13cda8bfa252dc178fe5d72
Will Pinto <mitrabajo45@gmail.com>
2025-10-19 17:13:01 -0500

    Corrige storageBucket en Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -6,11 +6,12 @@ const firebaseConfig = {
   apiKey: "AIzaSyBCTA4vMHpyIrYQKtYHqpGssWk8zrdTtEs",
   authDomain: "mialmacendb.firebaseapp.com",
   projectId: "mialmacendb",
-  storageBucket: "mialmacendb.firebasestorage.app",
+  storageBucket: "mialmacendb.appspot.com", // ← corregido aquí
   messagingSenderId: "335290511607",
   appId: "1:335290511607:web:9398cc0898f829d362a3f2",
   measurementId: "G-E2YNQKW1X3"
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
