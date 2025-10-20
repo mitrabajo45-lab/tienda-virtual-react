c02df02cf05333b43a3c0f1655e8ca88de522e6a
will <mitrabajo45@gmail.com>
2025-10-20 17:57:02 -0500

    Arreglar alias @ para Vite y corregir error 404 en Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"), // 👈 ahora Vite sabrá dónde buscar "@/..."
     },
   },
-})
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    port: 5173,
+  },
+});
